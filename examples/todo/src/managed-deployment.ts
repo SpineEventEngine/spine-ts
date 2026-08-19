75e4cadef50cad5412f1b1df3a11abc0e78f6c98
Alex Tymchenko <alex.tymchenko@teamdev.com>
2026-08-19 08:34:53 +0100

    fix(examples): satisfy managed entry preflight

--- examples/todo/src/managed-deployment.ts
+++ examples/todo/src/managed-deployment.ts
@@ -1,4 +1,18 @@
-/* Internal configuration for the To-Do managed executable. */
+/*
+ * Copyright 2026, CodeMatters. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+/** Internal configuration for the To-Do managed executable. */
 export interface TodoManagedDeployment {
   readonly host: string;
   readonly port: number;
@@ -8,6 +22,12 @@ export interface TodoManagedDeployment {
   readonly deliveryShardCount: number;
 }
 
+/**
+ * Reads the managed executable configuration.
+ *
+ * @param environment Supplies the process settings.
+ * @returns The validated managed deployment settings.
+ */
 export function readTodoManagedDeployment(environment: NodeJS.ProcessEnv): TodoManagedDeployment {
   return {
     host: required(environment, "HOST"),
